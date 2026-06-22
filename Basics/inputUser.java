1591cf35147d7eeb1ea1346588f59c9a8cfef10a
itsomrai <work.omrai@gmail.com>
2026-06-22 22:43:18 +0530

    Basics

---
+++ Basics/inputUser.java
@@ -0,0 +1,14 @@
+import java .util.Scanner;
+public class inputUser {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Input your Age :");
+        int age=sc.nextInt();
+        System.out.println(age);
+    }
+}
+git add .public class 
+ {
+
+    
+}
