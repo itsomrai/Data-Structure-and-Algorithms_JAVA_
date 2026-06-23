83a4dc0a6422d9dabab31f078d68c9bb004ed5ca
itsomrai <work.omrai@gmail.com>
2026-06-23 17:05:24 +0530

    UPDATED

--- Basics/conditional.java
+++ Basics/conditional.java
@@ -5,5 +5,13 @@ public class conditional {
             System.out.println("Day");
         else
             System.out.println("Night");
+
+
+        int age=18;;
+        if(age>=18)
+            System.out.println("You can vote");
+        else
+            System.out.println("Not elligibble to vote");
+
     }
 }
