d97e9ec40ac22ceef0654bcf3acc9f12fef30ba8
itsomrai <work.omrai@gmail.com>
2026-06-23 16:26:27 +0530

    BASICS

--- Basics/inputUser.java
+++ Basics/inputUser.java
@@ -1,9 +1,13 @@
-import java .util.Scanner;
+import java.util.Scanner;
 public class inputUser {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Input your Age :");
+        System.out.println("Input the name");
         int age=sc.nextInt();
+        String name=sc.nextLine();
+        System.out.println(name);
         System.out.println(age);
+
     }
 }
