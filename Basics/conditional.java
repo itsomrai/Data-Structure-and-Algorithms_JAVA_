fc5f45b1ecf0ec34ccb0203828f5803a49b7fcec
itsomrai <work.omrai@gmail.com>
2026-06-23 16:57:37 +0530

    BASICS

---
+++ Basics/conditional.java
@@ -0,0 +1,9 @@
+public class conditional {
+    public static void main(String[] args) {
+        boolean isSun=false;
+        if(isSun==true)
+            System.out.println("Day");
+        else
+            System.out.println("Night");
+    }
+}
