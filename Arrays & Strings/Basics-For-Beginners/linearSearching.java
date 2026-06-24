b0a7c46a368178530b80da94199d34d5b238060d
itsomrai <work.omrai@gmail.com>
2026-06-24 21:59:35 +0530

    UPDATED

--- Arrays & Strings/Basics-For-Beginners/linearSearching.java
+++ Arrays & Strings/Basics-For-Beginners/linearSearching.java
@@ -1,17 +1,18 @@
 public class linearSearching {
 
-    public static int linearSearch(int[] arr, int target){
+    public static int linearsearch(int[] arr , int target){
         for(int i=0; i<=arr.length;i++){
             if(arr[i]==target){
                 return i;
             }
-}
-return -1;
+        }
+        return -1;
     }
     public static void main(String[] args) {
-        int[] numbers={5,4,3,6,7,8};
-        int target=3;
-        int result= linearSearch(numbers, target);
-        System.out.println("Element found at index:" + result);
+        int[] number={5,6,7,8,3,4};
+            int target=4;
+            int result= linearsearch(number, target);
+            System.out.println("Element found at index:" + result);
+        }
     }
-}
+
