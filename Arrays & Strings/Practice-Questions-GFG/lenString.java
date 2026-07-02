e3d84f0ba3a2f9650d0879539accbb30aa9fb3e9
itsomrai <work.omrai@gmail.com>
2026-07-02 18:14:00 +0530

    GFG PRACTICE

---
+++ Arrays & Strings/Practice-Questions-GFG/lenString.java
@@ -0,0 +1,8 @@
+public class lenString {
+    public static void main(String[] args) {
+        String name="OM RAI";
+        int len=name.length();
+        System.out.println("LENGTH OF NAME IS :"+len);
+    }
+    
+}
