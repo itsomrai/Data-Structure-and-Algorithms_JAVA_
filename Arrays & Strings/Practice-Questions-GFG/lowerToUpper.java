6a6e2016738ba40685241aff597a4faafef510f4
itsomrai <work.omrai@gmail.com>
2026-07-04 17:44:05 +0530

    GFG PRACTICE

---
+++ Arrays & Strings/Practice-Questions-GFG/lowerToUpper.java
@@ -0,0 +1,7 @@
+public class lowerToUpper {
+    public static void main(String[] args) {
+        String str="my name is om rai";
+        String s=str.toUpperCase();
+        System.out.println(s);
+    }
+}
