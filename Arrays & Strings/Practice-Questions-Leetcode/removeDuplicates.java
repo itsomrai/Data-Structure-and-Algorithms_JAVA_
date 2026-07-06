b9ad5aded863617b0e6edf109d729f848b04d004
itsomrai <work.omrai@gmail.com>
2026-07-06 23:11:02 +0530

    LEETCODE PRACTICE

---
+++ Arrays & Strings/Practice-Questions-Leetcode/removeDuplicates.java
@@ -0,0 +1,14 @@
+import java . util .Arrays;
+public class removeDuplicates {
+    public static void main(String[] args) {
+        int[] num={1,1,2,2,3,3,4,4,5,5};
+        System.out.print(num[0]+" ");
+
+        for(int i=1;i<num.length;i++){
+            if(num[i]!=num[i-1]){
+            System.out.print(num[i]+ " ");
+
+            }
+        }
+    }
+}
