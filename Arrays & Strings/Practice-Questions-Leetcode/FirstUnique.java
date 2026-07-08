238c85744bb9b8f97195f49fe681e6d948e6f69f
itsomrai <work.omrai@gmail.com>
2026-07-08 13:29:38 +0530

    LEETCODE PRACTICE

---
+++ Arrays & Strings/Practice-Questions-Leetcode/FirstUnique.java
@@ -0,0 +1,19 @@
+public class FirstUnique {
+    public static int firstUnique(String s){
+        int[] freq=new int[26];
+        for(int i=0;i<s.length();i++){
+            freq[s.charAt(i)-'a']++;
+        }
+
+        for(int i=0;i<s.length();i++){
+            if(freq[s.charAt(i)-'a']==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        String s="loveleetcode";
+        System.out.println(firstUnique(s));
+    }
+}
