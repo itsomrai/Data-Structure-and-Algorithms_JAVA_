0c2a447c1cf418b76689a81d55c275bb8359614f
itsomrai <work.omrai@gmail.com>
2026-07-11 19:26:47 +0530

    UPDATED

--- Arrays & Strings/Practice-Questions-Leetcode/arraySort.java
+++ Arrays & Strings/Practice-Questions-Leetcode/arraySort.java
@@ -1,9 +1,10 @@
 import java.util.Arrays;
 public class arraySort {
 public static void main(String[] args) {
-int[] nums={5,4,3,2,1};
-    for(int i=0;i<nums.length;i++){
-    for(int j=0;j<nums.length-1-i;j++){
+int[] nums={5,3,4,2,1};
+int len=nums.length;
+    for(int i=0;i<len;i++){
+    for(int j=0;j<len-1-i;j++){
         if(nums[j]>nums[j+1]){
             int temp=nums[j];
             nums[j]=nums[j+1];
