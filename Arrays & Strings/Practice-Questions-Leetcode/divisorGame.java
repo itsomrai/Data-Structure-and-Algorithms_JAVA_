b5240470d0d2f415d48686978c0deb2c6720a12c
itsomrai <work.omrai@gmail.com>
2026-07-11 19:49:31 +0530

    LEETCODE PRACTICE

---
+++ Arrays & Strings/Practice-Questions-Leetcode/divisorGame.java
@@ -0,0 +1,11 @@
+public class divisorGame {
+    public static void main(String[] args) {
+        int n=56;
+        if(n%2==0){
+            System.out.println("true");
+        }
+        else{
+            System.out.println("falseg");
+        }
+    }
+}
