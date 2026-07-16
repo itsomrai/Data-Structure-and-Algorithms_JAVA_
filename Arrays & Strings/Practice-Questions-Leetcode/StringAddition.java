16a579d2284b8283953fb6be9e3f29b73640682f
itsomrai <work.omrai@gmail.com>
2026-07-16 15:40:15 +0530

    LEETCODE PRACTICE

---
+++ Arrays & Strings/Practice-Questions-Leetcode/StringAddition.java
@@ -0,0 +1,9 @@
+public class StringAddition {
+    public static void main(String[] args) {
+        String num1="12345";
+        String num2="11345";
+        int sum=Integer.parseInt(num1)+Integer.parseInt(num2);
+        String res=String.valueOf(sum);
+        System.out.println(res);
+    }
+}
