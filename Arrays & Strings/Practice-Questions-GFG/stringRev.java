1c2846ef253a11c913e21a0109b5dc72471ae193
itsomrai <work.omrai@gmail.com>
2026-07-25 21:25:38 +0530

    GFG PROBELUMS

--- Arrays & Strings/Practice-Questions-GFG/stringRev.java
+++ Arrays & Strings/Practice-Questions-GFG/stringRev.java
@@ -1,4 +1,4 @@
-public class revString {
+public class stringRev {
     public static void main(String[] args) {
         String s="HELLO";
         String rev="";
@@ -8,3 +8,4 @@ public class revString {
         System.out.println("REVERSED STRING IS: "+rev);
     }
 }
+
