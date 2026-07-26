c4cdfaed712bf1a9cd0ed7f13b3b8eb53efebde5
itsomrai <work.omrai@gmail.com>
2026-07-26 22:24:12 +0530

    LEETCODE PRACTICE

---
+++ Arrays & Strings/Practice-Questions-Leetcode/maxproductthree.java
@@ -0,0 +1,10 @@
+public class maxproductthree {
+    public static void main(String[] args) {
+        int[] nums={1,2,3};
+        int pro=1;
+        for(int i=0;i<nums.length;i++){
+            pro*=nums[i];
+        }
+        System.out.println(pro);
+    }
+}
