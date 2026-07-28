eae95cc62db68d3f97df689b234e1db7eea2f35e
itsomrai <work.omrai@gmail.com>
2026-07-28 17:10:33 +0530

    LEETCODE PRACTICE

---
+++ Arrays & Strings/Practice-Questions-Leetcode/stringLower.java
@@ -0,0 +1,7 @@
+public class stringLower {
+    public static void main(String[] args) {
+        String s="HELLO WORLD";
+        String low=s.toLowerCase();
+        System.out.println(low);
+    }
+}
