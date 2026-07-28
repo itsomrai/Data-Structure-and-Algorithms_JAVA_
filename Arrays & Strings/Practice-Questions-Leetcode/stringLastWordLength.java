b39961edc8434baa59b02ebd420cb0fb9a729a01
itsomrai <work.omrai@gmail.com>
2026-07-28 17:14:35 +0530

    LEETCODE PRACTICE

---
+++ Arrays & Strings/Practice-Questions-Leetcode/stringLastWordLength.java
@@ -0,0 +1,15 @@
+public class stringLastWordLength {
+    public static void main(String[] args) {
+        String s="Hello My NAme Is OM";
+        int length=0;
+        for(int i=s.length()-1;i>=0;i--){
+            if(s.charAt(i)!=' '){
+                length++;
+            }
+            else if(length>0){
+                break;
+            }
+        }
+        System.out.println(length);
+    }
+}
