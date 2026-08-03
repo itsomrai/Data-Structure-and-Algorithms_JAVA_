67026ce816a57016b9f7330208d216569f952391
itsomrai <work.omrai@gmail.com>
2026-08-03 21:21:49 +0530

    GFG PRACTCIE

--- Arrays & Strings/Practice-Questions-GFG/peakValue.java
+++ Arrays & Strings/Practice-Questions-GFG/peakValue.java
@@ -1,16 +1,17 @@
 public class peakValue {
     public static void main(String[] args) {
-        int[] arr={1, 2, 4, 5, 7, 8, 9};
-        for(int i=0;i<arr.length;i++){
+        int[] arr={1, 2, 4, 5, 7, 8, 3};
+        boolean foundPeak=false;
+        for(int i=0;i<arr.length-1;i++){
             if(arr[i]>arr[i+1] && arr[i]>arr[i-1]){
                 System.out.println("true");
+                foundPeak=true;
                 return;
             }
-            else{
-        System.out.println("false");
-        return;
 
+            }
+            if(!foundPeak){
+                System.out.println("false");
             }
         }
     }
-}
